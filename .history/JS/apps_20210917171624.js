959b302ac5de03133bab95ebfac588b017433a4a
Sarah <74516787+Sbethm@users.noreply.github.com>
2021-09-17 17:35:23 -0500

    UX ticker styling
    adjusting the ticker's countdown styling, so that when it is counting down, it moves in a way that users expect

---
+++ .history/JS/apps_20210917171624.js
@@ -0,0 +1,81 @@
+const playBtn = document.querySelector('.fa-play-circle ');
+const pauseBtn = document.querySelector('.fa-pause-circle ');
+const input = document.querySelector('input'); 
+const ticker = document.querySelector('circle');
+
+//Reusable Timer Class
+let countDownNum = ticker.style.strokeDasharray;
+
+function tickDown() {
+            console.log(ticker.style.strokeDashoffset = `${countDownNum};`);
+            
+            // countDownNum = countDownNum - 1;
+        }
+
+const timer1 = new Timer(input, playBtn, pauseBtn, {
+    onTick() {
+        tickDown();
+    },
+    
+    onStart() {
+        playBtn.classList.remove('active');
+        pauseBtn.classList.add('active');
+    },
+
+    onPause() {
+        playBtn.classList.remove('active');
+        pauseBtn.classList.add('active');
+    },
+
+    onComplete() {
+
+    }
+});
+
+
+
+
+
+
+
+
+// //FUNCTIONS
+// function startTimer() {
+//     let inputNum = document.querySelector('input').value;
+
+//     let countDownNum = Number(inputNum);
+
+//     if(!countDownNum || countDownNum !== Math.floor(countDownNum)) {
+//         console.log("WHOLE NUMBERS ONLY!")
+//     } else {
+//         countDownNum--;
+//         inputNum = countDownNum;
+//         console.log(inputNum);
+//     }
+// }
+
+// function pauseTimer() {
+
+// }
+
+
+
+// playBtn.addEventListener('click', () => {
+//     //Btn style change
+//     playBtn.classList.remove('active');
+//     pauseBtn.classList.add('active');
+
+//     //Input countdown
+//     startTimer()
+    
+    
+// })
+
+// pauseBtn.addEventListener('click', () => {
+//     //Btn style change
+//     pauseBtn.classList.remove('active');
+//     playBtn.classList.add('active');
+
+//     //Pause Timer
+//     pauseTimer();
+// })
---
+++ .history/JS/timerClass_20210917170043.js
@@ -0,0 +1,52 @@
+class Timer {
+    constructor(durationInput, playBtn, pauseBtn, callbacks) {
+        this.durationInput = durationInput;
+        this.playBtn = playBtn;
+        this.pauseBtn = pauseBtn;
+        this.onStart = callbacks.onStart;
+        this.onPause = callbacks.onPause;
+        this.onComplete = callbacks.onComplete;
+
+
+        this.playBtn.addEventListener('click', this.start);
+        this.pauseBtn.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        this.onStart();
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+        
+        
+        playBtn.classList.remove('active');
+        pauseBtn.classList.add('active');
+        // for(let i = durationInput; durationInput > 0; i--){
+        //     setTimeout(this.tick, 1000);
+        // }
+    };
+
+    pause = () => {
+        this.onPause();
+        clearInterval(this.interval);
+
+        pauseBtn.classList.remove('active');
+        playBtn.classList.add('active');
+    };
+
+    tick = () => {
+        if(this.timeRemaining > 0){
+            this
+            this.timeRemaining = this.timeRemaining - 1;
+        } else {
+            this.pause();
+        }
+        
+    };
+
+    get timeRemaining () {
+        return parseFloat(this.durationInput.value);
+    }
+    set timeRemaining (time) {
+        this.durationInput.value = time;
+    }
+}
